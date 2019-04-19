da5899dd9d8a6438de516ddf2b3a46fe1bf84e0d
Wally Jones <wally@wallyjones.com>
2019-04-18 23:42:40 -0400

    Add two new versions of IsUnique()

--- ctci/1-1.go
+++ ctci/1-1.go
@@ -1,12 +1,11 @@
 // Is Unique: Implement an algorithm to determine if a string has all
 // unique characters. What if you cannot use additional data
 // structures?
-
 package main
 
 import "fmt"
 
-// Using additional data structures
+// Using additional data structures (map or hash table)
 func IsUnique1(s string) bool {
 	seen := make(map[rune]int)
 	for _, r := range s {
@@ -32,10 +31,27 @@ func IsUnique2(s string) bool {
 	return true
 }
 
+// Is an array considered an additional data structure?
+func IsUnique3(s string) bool {
+	seen := make([]int, 1024)
+
+	for _, r := range s {
+		if seen[int(r)] == 1 {
+			return false
+		}
+		seen[int(r)] = 1
+	}
+
+	return true
+}
+
 func main() {
 	fmt.Println(IsUnique1("foobar"))
 	fmt.Println(IsUnique1("abcdefghijklmnopqrstuvwxyz"))
-
+	fmt.Println("---")
 	fmt.Println(IsUnique2("foobar"))
 	fmt.Println(IsUnique2("abcdefghijklmnopqrstuvwxyz"))
+	fmt.Println("---")
+	fmt.Println(IsUnique3("foobar"))
+	fmt.Println(IsUnique3("abcdefghijklmnopqrstuvwxyz"))
 }
