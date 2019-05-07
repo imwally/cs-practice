e22716baab78d5ac709589194f121c41a0833262
Wally Jones <wally@wallyjones.com>
2019-05-07 00:37:19 -0400

    This somewhat works

--- ctci/1-7_test.go
+++ ctci/1-7_test.go
@@ -1,6 +1,6 @@
 // Given an image represented by an NxN matrix, where each pixel in
 // the image is 4 bytes, write a method to rotate the image by 90
-// degrees. (an you do this in place?
+// degrees. Can you do this in place?
 
 package main
 
@@ -9,24 +9,32 @@ import (
 	"testing"
 )
 
-func RotateMatrix(m [][]int) {
-	for i := 0; i < len(m)-1; i++ {
-		for j := 0; j < len(m)-1; j++ {
-			temp := m[i][j+1]
-			m[i][j+1] = m[i+1][j]
-			m[i+1][j] = temp
+const SIZE = 4
+
+type matrix [SIZE][SIZE]int
+
+func RotateMatrix(m matrix) matrix {
+	w := len(m)
+	var rotated matrix
+	for i := 0; i < w; i++ {
+		for j := 0; j < w; j++ {
+			rotated[i][j] = m[w-j-1][i]
 		}
 	}
+
+	return rotated
 }
 
 func TestRotateMatrix(t *testing.T) {
 
-	m := [][]int{
-		{1, 2},
-		{3, 4},
+	m := matrix{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16},
 	}
 
 	fmt.Println(m)
-	RotateMatrix(m)
+	m = RotateMatrix(m)
 	fmt.Println(m)
 }
