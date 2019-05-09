82c8bbb0b625312359ccfa5a45f442757cc0b4bf
Wally Jones <wally@wallyjones.com>
2019-05-08 21:45:33 -0400

    Add tests for 1-7

--- ctci/1-7_test.go
+++ ctci/1-7_test.go
@@ -4,15 +4,13 @@
 
 package main
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 const SIZE = 4
 
 type matrix [SIZE][SIZE]int
 
+// This is not done in place and uses a fixed size matrix.
 func RotateMatrix(m matrix) matrix {
 	w := len(m)
 	var rotated matrix
@@ -26,7 +24,6 @@ func RotateMatrix(m matrix) matrix {
 }
 
 func TestRotateMatrix(t *testing.T) {
-
 	m := matrix{
 		{1, 2, 3, 4},
 		{5, 6, 7, 8},
@@ -34,7 +31,32 @@ func TestRotateMatrix(t *testing.T) {
 		{13, 14, 15, 16},
 	}
 
-	fmt.Println(m)
-	m = RotateMatrix(m)
-	fmt.Println(m)
+	answer := matrix{
+		{13, 9, 5, 1},
+		{14, 10, 6, 2},
+		{15, 11, 7, 3},
+		{16, 12, 8, 4},
+	}
+
+	if RotateMatrix(m) != answer {
+		t.Errorf("%v should be\n %v after rotation", m, answer)
+	}
+
+	m = matrix{
+		{1, 2, 3, 4},
+		{1, 2, 3, 4},
+		{1, 2, 3, 4},
+		{1, 2, 3, 4},
+	}
+
+	answer = matrix{
+		{1, 1, 1, 1},
+		{2, 2, 2, 2},
+		{3, 3, 3, 3},
+		{4, 4, 4, 4},
+	}
+
+	if RotateMatrix(m) != answer {
+		t.Errorf("%v should be\n %v after rotation", m, answer)
+	}
 }
