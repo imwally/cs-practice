5396e1e7deb45530e48d2eae81bdec0aa60149ac
Wally Jones <wally@wallyjones.com>
2019-05-23 22:03:36 -0400

    Add doubly linked list

---
+++ data-structures/doublylinkedlist.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Value interface{}
+	Next  *Node
+	Prev  *Node
+}
+
+type LinkedList struct {
+	Head *Node
+	Tail *Node
+}
+
+func (l *LinkedList) Append(v interface{}) {
+	node := &Node{v, nil, nil}
+	if l.Head == nil {
+		l.Head = node
+		l.Tail = node
+
+		return
+	}
+
+	node.Prev = l.Tail
+	l.Tail.Next = node
+	l.Tail = node
+}
+
+func (l *LinkedList) String() string {
+	var list string
+	for current := l.Head; current != nil; current = current.Next {
+		list += fmt.Sprintf("%v ", current.Value)
+		if current.Next != nil {
+			list += fmt.Sprintf("-> ")
+		}
+	}
+
+	return list
+}
+
+func PrintReversed(l *LinkedList) {
+	for current := l.Tail; current != nil; current = current.Prev {
+		fmt.Printf("%v ", current.Value)
+		if current.Prev != nil {
+			fmt.Printf("-> ")
+		}
+	}
+	fmt.Println()
+}
+
+func main() {
+	ll := &LinkedList{nil, nil}
+	ll.Append(1)
+	ll.Append(2)
+	ll.Append(3)
+	ll.Append(4)
+
+	fmt.Println(ll)
+	PrintReversed(ll)
+}
