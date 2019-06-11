62b72e12b2a65886bb130cacde2b0df9e07323a7
Wally Jones <wally@wallyjones.com>
2019-06-11 11:11:51 -0400

    Use Push / Pop and add comments

--- data-structures/heap/heap.go
+++ data-structures/heap/heap.go
@@ -17,6 +17,11 @@ func New() *Heap {
 	return &Heap{data, 0}
 }
 
+// HeapifyUp starts at the last element in the array and works
+// backwards from child to parent checking if the value is smaller
+// than its parent. If the child is smaller than the parent, they are
+// swapped. This happens each time a new element is pushed onto the
+// heap to maintain order.
 func (h *Heap) HeapifyUp() {
 	child := h.NextIndex
 	for child > 0 {
@@ -28,20 +33,31 @@ func (h *Heap) HeapifyUp() {
 	}
 }
 
+// HeapifyDown starts at the root (or first element of the data array)
+// and works downwards checking the parent and both children to find
+// the smallest value among the three. The smallest value will be
+// swapped with the parent. This happens each time an element is
+// popped off the heap to maintain order.
 func (h *Heap) HeapifyDown() {
-	p := 0
-
-	for p < h.NextIndex {
+	for p := 0; p < h.NextIndex; {
+		// Left and right child
 		l, r := 2*p+1, 2*p+2
+
+		// Smallest and its index
 		s, si := h.Data[p], p
 
+		// Check if left child is smaller than parent, if so
+		// update smallest and its index
 		if l < h.NextIndex && h.Data[l] < s {
 			s, si = h.Data[l], l
 		}
+
+		// Check if right is smaller than smallest seen so far
 		if r < h.NextIndex && h.Data[r] < s {
 			s, si = h.Data[r], r
 		}
 
+		// If parent is already the smallest do nothing
 		if p == si {
 			return
 		}
@@ -51,7 +67,11 @@ func (h *Heap) HeapifyDown() {
 	}
 }
 
-func (h *Heap) Insert(v int) {
+// Push inserts a new value into the data array and then calls
+// HeapifyUp to maintain order. If the array is full the size will be
+// doubled and its contents copied.
+func (h *Heap) Push(v int) {
+	// Resize array if full
 	if h.NextIndex == len(h.Data) {
 		newData := make([]int, len(h.Data)*2)
 		copy(newData, h.Data)
@@ -63,7 +83,14 @@ func (h *Heap) Insert(v int) {
 	h.NextIndex++
 }
 
-func (h *Heap) Remove() int {
+// Pop removes and returns the root (or first element of the data
+// array) from the heap. The root and last element are swapped then
+// HeapifyDown is called to maintain order.
+func (h *Heap) Pop() int {
+	if h.NextIndex == 0 {
+		panic("stack underflow")
+	}
+
 	h.NextIndex--
 	root := h.Data[0]
 	swap(h.Data, 0, h.NextIndex)
--- data-structures/heap/heap_test.go
+++ data-structures/heap/heap_test.go
@@ -4,44 +4,44 @@ import (
 	"testing"
 )
 
-func TestInsert(t *testing.T) {
+func TestPush(t *testing.T) {
 	h := New()
 
 	expected := 10
-	h.Insert(expected)
+	h.Push(expected)
 	got := h.Data[0]
 	if got != expected {
-		t.Errorf("Insert failed: expected %d, got %d", expected, got)
+		t.Errorf("Push failed: expected %d, got %d", expected, got)
 	}
 
 	expected = 5
-	h.Insert(expected)
+	h.Push(expected)
 	got = h.Data[0]
 	if got != expected {
-		t.Errorf("Insert failed: expected %d, got %d", expected, got)
+		t.Errorf("Push failed: expected %d, got %d", expected, got)
 	}
 
 	expected = 1
-	h.Insert(expected)
+	h.Push(expected)
 	got = h.Data[0]
 	if got != expected {
-		t.Errorf("Insert failed: expected %d, got %d", expected, got)
+		t.Errorf("Push failed: expected %d, got %d", expected, got)
 	}
 }
 
-func TestRemove(t *testing.T) {
+func TestPop(t *testing.T) {
 	h := New()
-	size := 10
+	size := 40
 
 	for i := size; i > 0; i-- {
-		h.Insert(i)
+		h.Push(i)
 	}
 
 	for i := 1; i <= size; i++ {
 		expected := i
-		got := h.Remove()
+		got := h.Pop()
 		if got != expected {
-			t.Errorf("Remove failed: expected %d, got %d", expected, got)
+			t.Errorf("Pop failed: expected %d, got %d", expected, got)
 		}
 	}
 }
