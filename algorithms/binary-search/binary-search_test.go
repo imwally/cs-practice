d4a5c2724d4bdf131e537f274afd723df275fbd7
Wally Jones <wally@wallyjones.com>
2019-07-06 20:39:41 -0400

    Use testing instead

---
+++ algorithms/binary-search/binary-search_test.go
@@ -0,0 +1,46 @@
+package main
+
+import "testing"
+
+func find(x int, slice []int) int {
+	i, j := 0, len(slice)-1
+
+	for i <= j {
+		mid := int((i + j) / 2)
+		if x == slice[mid] {
+			return slice[mid]
+		}
+		if x < slice[mid] {
+			j = mid - 1
+		} else {
+			i = mid + 1
+		}
+	}
+
+	return -1
+}
+
+func TestFind(t *testing.T) {
+	var slice []int
+	for i := 1; i <= 1000000; i++ {
+		slice = append(slice, i)
+	}
+
+	got := find(8270, slice)
+	expected := 8270
+	if got != expected {
+		t.Errorf("find error: got %d, expected %d\n", got, expected)
+	}
+
+	got = find(1, slice)
+	expected = 1
+	if got != expected {
+		t.Errorf("find error: got %d, expected %d\n", got, expected)
+	}
+
+	got = find(999999, slice)
+	expected = 999999
+	if got != expected {
+		t.Errorf("find error: got %d, expected %d\n", got, expected)
+	}
+}
