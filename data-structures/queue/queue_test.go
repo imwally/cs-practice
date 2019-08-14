f3e9c54de71a87aedc2d6294e3081091932cfe9d
Wally Jones <wally@wallyjones.com>
2019-08-13 21:15:51 -0400

    Add Dequeue test

--- data-structures/queue/queue_test.go
+++ data-structures/queue/queue_test.go
@@ -18,3 +18,20 @@ func TestEnqueue(t *testing.T) {
 		}
 	}
 }
+
+func TestDequeue(t *testing.T) {
+	q := New()
+
+	for i := 1; i <= 10; i++ {
+		q.Enqueue(i)
+	}
+
+	for i := 1; i <= 10; i++ {
+		expected := i
+		got := q.Dequeue()
+
+		if got != expected {
+			t.Errorf("Dequeue error: got %v, expected %v", got, expected)
+		}
+	}
+}
