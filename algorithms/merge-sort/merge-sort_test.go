622a18424d791445b82882b6d2facdc16c6cd9cc
Wally Jones <wally@wallyjones.com>
2019-08-15 23:34:45 -0400

    Add merge sort

---
+++ algorithms/merge-sort/merge-sort_test.go
@@ -0,0 +1,53 @@
+package main
+
+import "testing"
+
+func Merge(a, b []int) []int {
+	merged := make([]int, len(a)+len(b))
+
+	i, j, k := 0, 0, 0
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			merged[k] = a[i]
+			i++
+		} else {
+			merged[k] = b[j]
+			j++
+		}
+		k++
+	}
+
+	copy(merged[k:], a[i:])
+	copy(merged[k:], b[j:])
+
+	return merged
+}
+
+func MergeSort(s []int) []int {
+	if len(s) <= 1 {
+		return s
+	}
+
+	mid := int(len(s) / 2)
+	a := s[:mid]
+	b := s[mid:]
+
+	return Merge(MergeSort(a), MergeSort(b))
+}
+
+func TestMergeSort(t *testing.T) {
+	var usort []int
+	for i := 100; i > 0; i-- {
+		usort = append(usort, i)
+	}
+
+	sorted := MergeSort(usort)
+	for i := 0; i < 100; i++ {
+		expected := i + 1
+		got := sorted[i]
+
+		if got != expected {
+			t.Errorf("MergeSort error: got %v, expected %v", got, expected)
+		}
+	}
+}
