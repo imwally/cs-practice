426c9e96370406d6cddb833b3f73352587f17695
Wally Jones <wally@wallyjones.com>
2019-09-12 23:45:01 -0400

    Add recursive Flatten function

--- data-structures/linkedlist/linkedlist_flatten_test.go
+++ data-structures/linkedlist/linkedlist_flatten_test.go
@@ -18,7 +18,7 @@ func Merge(a, b *LinkedList) *LinkedList {
 	ac := a.Head
 	bc := b.Head
 
-        // Merge lists in order
+	// Merge lists in order
 	for ac != nil && bc != nil {
 		if ac.Value.(int) <= bc.Value.(int) {
 			merged.Append(ac.Value)
@@ -29,20 +29,27 @@ func Merge(a, b *LinkedList) *LinkedList {
 		}
 	}
 
-        // Add remaining elements from b list
-        for bc != nil {
-                merged.Append(bc.Value)
-                bc = bc.Next
-        }
+	// Add remaining elements from b list
+	for bc != nil {
+		merged.Append(bc.Value)
+		bc = bc.Next
+	}
 
-        // Add remaining elements from a list
-        for ac != nil {
-                merged.Append(ac.Value)
-                ac = ac.Next
-        }
-
+	// Add remaining elements from a list
+	for ac != nil {
+		merged.Append(ac.Value)
+		ac = ac.Next
+	}
 
 	return merged
+}
+
+func Flatten(n *Node) *LinkedList {
+	if n.Next == nil {
+		return Merge(n.Value.(*LinkedList), nil)
+	}
+
+	return Merge(n.Value.(*LinkedList), Flatten(n.Next))
 }
 
 func TestFlatten(t *testing.T) {
@@ -61,9 +68,14 @@ func TestFlatten(t *testing.T) {
 	llb.Append(8)
 	llb.Append(9)
 
-	m := Merge(Merge(llb, llc), lld)
+	lla := &LinkedList{}
+	lla.Append(llb)
+	lla.Append(llc)
+	lla.Append(lld)
 
-	got := fmt.Sprintf("%s", m)
+	flat := Flatten(lla.Head)
+
+	got := fmt.Sprintf("%s", flat)
 	expected := "1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9"
 	if got != expected {
 		t.Errorf("Flatten error: got %v, expected %v", got, expected)
