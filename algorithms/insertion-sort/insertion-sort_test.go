a336de14dcfd7f17b14d0f7fbdc5b9593ff0c0a9
Wally Jones <wally@wallyjones.com>
2019-09-27 11:50:47 -0400

    Add comments and condense for loop

--- algorithms/insertion-sort/insertion-sort_test.go
+++ algorithms/insertion-sort/insertion-sort_test.go
@@ -3,11 +3,12 @@ package main
 import "testing"
 
 func InsertionSort(s []int) {
+	// Loop over every item in the slice
 	for i := 0; i < len(s); i++ {
-		j := i
-		for j > 0 && s[j-1] > s[j] {
+		// Starting at the i'th element, if the i-1 element is larger,
+		// swap them
+		for j := i; j > 0 && s[j-1] > s[j]; j-- {
 			s[j], s[j-1] = s[j-1], s[j]
-			j--
 		}
 	}
 }
