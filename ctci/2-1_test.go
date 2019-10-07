ac347b2c26d3b04d2f455eca5ff6ff65bc31f807
Wally Jones <wally@wallyjones.com>
2019-10-07 14:45:40 -0400

    Check for negative k or if k is greater than size of list

--- ctci/2-1_test.go
+++ ctci/2-1_test.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"errors"
 	"testing"
 
 	"../data-structures/linkedlist"
 )
 
-func KthToLast(ll *linkedlist.LinkedList, k int) int {
+func KthToLast(ll *linkedlist.LinkedList, k int) (int, error) {
+	if k < 0 || k > ll.Size()-1 {
+		return 0, errors.New("error: %d out of bounds")
+	}
+
 	current := ll.Head
 	for i := 1; i < ll.Size()-k; i++ {
 		current = current.Next
 	}
 
-	return current.Value.(int)
+	return current.Value.(int), nil
 }
 
 func TestKthToLast(t *testing.T) {
@@ -23,7 +28,11 @@ func TestKthToLast(t *testing.T) {
 	l.Append(3)
 	l.Append(4)
 
-	got := KthToLast(l, 2)
+	got, err := KthToLast(l, 2)
+	if err != nil {
+		t.Error(err)
+	}
+
 	if got != 2 {
 		t.Errorf("error: expected 2, got %d\n", got)
 	}
