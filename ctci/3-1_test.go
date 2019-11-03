56d9fb52dc1c4af49b9b27720d3bf3983bb678d4
Wally Jones <wally@wallyjones.com>
2019-11-02 23:15:37 -0400

    Test stacks

--- ctci/3-1_test.go
+++ ctci/3-1_test.go
@@ -6,91 +6,100 @@
 
 package main
 
-import (
-        "testing"
-        "fmt"
-)
+import "testing"
 
 type Stacks struct {
-        Storage []int
-        Capacity int
-        One int
-        Two int
-        Three int
+	Storage  []int
+	Capacity int
+	One      int
+	Two      int
+	Three    int
 }
 
 func New() *Stacks {
-        cap := 30
-        storage := make([]int, cap)
-        return &Stacks{
-                Storage: storage,
-                Capacity: cap,
-                One: 0,
-                Two: 0,
-                Three: 0,
-        }
+	cap := 30
+	storage := make([]int, cap)
+	return &Stacks{
+		Storage:  storage,
+		Capacity: cap,
+		One:      0,
+		Two:      0,
+		Three:    0,
+	}
 }
 
-
 func (s *Stacks) Push(which, v int) {
-        i := 0
-        switch which {
-        case 1:
-                i = s.One
-                s.One++
-        case 2:
-                i = (s.Capacity / 3) + s.Two
-                s.Two++
-        case 3:
-                i = (s.Capacity / 3) + (s.Capacity / 3) + s.Three
-                s.Three++
-        }
+	i := 0
+	switch which {
+	case 1:
+		i = s.One
+		s.One++
+	case 2:
+		i = (s.Capacity / 3) + s.Two
+		s.Two++
+	case 3:
+		i = (s.Capacity / 3) + (s.Capacity / 3) + s.Three
+		s.Three++
+	}
 
-        s.Storage[i] = v
+	s.Storage[i] = v
 }
 
 func (s *Stacks) Pop(which int) int {
-        i := 0
-        switch which {
-        case 1:
-                s.One--
-                i = s.One
-        case 2:
-                s.Two--
-                i = (s.Capacity / 3) + s.Two
-        case 3:
-                s.Three--
-                i = (s.Capacity / 3) + (s.Capacity / 3) + s.Three
-        }
+	i := 0
+	switch which {
+	case 1:
+		s.One--
+		i = s.One
+	case 2:
+		s.Two--
+		i = (s.Capacity / 3) + s.Two
+	case 3:
+		s.Three--
+		i = (s.Capacity / 3) + (s.Capacity / 3) + s.Three
+	}
 
-        return s.Storage[i]
+	return s.Storage[i]
 }
 
 func TestPush(t *testing.T) {
 
-        stacks := New()
+	stacks := New()
 
-        // Testing Stack 1
-        stacks.Push(1, 1)
-        stacks.Push(1, 2)
-        stacks.Push(1, 3)
+	// Testing Stack 1
+	stacks.Push(1, 1)
+	stacks.Push(1, 2)
+	stacks.Push(1, 3)
 
-        // Testing Stack 2
-        stacks.Push(2, 10)
-        stacks.Push(2, 11)
-        stacks.Push(2, 12)
+	expected := 3
+	got := stacks.Pop(1)
 
+	if got != expected {
+		t.Errorf("Pop error: got %v, expected %v", got, expected)
+	}
 
-        // Testing Stack 3
-        stacks.Push(3, 20)
-        stacks.Push(3, 21)
-        stacks.Push(3, 22)
+	// Testing Stack 2
+	stacks.Push(2, 10)
+	stacks.Push(2, 11)
+	stacks.Push(2, 12)
 
-        fmt.Println(stacks)
+	expected = 12
+	got = stacks.Pop(2)
 
+	if got != expected {
+		t.Errorf("Pop error: got %v, expected %v", got, expected)
+	}
 
-        fmt.Println(stacks.Pop(3))
-        fmt.Println(stacks.Pop(2))
-        fmt.Println(stacks.Pop(1))
+	// Testing Stack 3
+	stacks.Push(3, 20)
+	stacks.Push(3, 21)
+	stacks.Push(3, 22)
+
+	expected = 22
+	got = stacks.Pop(3)
+
+	if got != expected {
+		t.Errorf("Pop error: got %v, expected %v", got, expected)
+	}
 
 }
