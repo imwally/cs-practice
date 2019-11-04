25f116fb42ea62c6f66b388a7a980ef0d4b7dce9
Wally Jones <wally@wallyjones.com>
2019-11-04 17:46:05 -0500

    Test Push() by scanning the backend storage slice

--- ctci/3-1_test.go
+++ ctci/3-1_test.go
@@ -6,10 +6,7 @@
 
 package main
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 type Stacks struct {
 	Storage  []int
@@ -130,68 +127,38 @@ func TestPush(t *testing.T) {
 	stacks.Push(1, 1)
 	stacks.Push(1, 2)
 	stacks.Push(1, 3)
+	for i := 0; i < 3; i++ {
+		expected := i + 1
+		got := stacks.Storage[i]
 
-	expected := 3
-	got := stacks.Pop(1)
-
-	if got != expected {
-		t.Errorf("Pop error: got %v, expected %v", got, expected)
+		if got != expected {
+			t.Errorf("Pop error: got %v, expected %v", got, expected)
+		}
 	}
 
 	// Testing Stack 2
 	stacks.Push(2, 11)
 	stacks.Push(2, 12)
 	stacks.Push(2, 13)
+	for i := 10; i < 13; i++ {
+		expected := i + 1
+		got := stacks.Storage[i]
 
-	expected = 13
-	got = stacks.Pop(2)
-
-	if got != expected {
-		t.Errorf("Pop error: got %v, expected %v", got, expected)
+		if got != expected {
+			t.Errorf("Pop error: got %v, expected %v", got, expected)
+		}
 	}
 
 	// Testing Stack 3
-	stacks.Push(3, 20)
 	stacks.Push(3, 21)
 	stacks.Push(3, 22)
+	stacks.Push(3, 23)
+	for i := 20; i < 23; i++ {
+		expected := i + 1
+		got := stacks.Storage[i]
 
-	expected = 22
-	got = stacks.Pop(3)
-
-	if got != expected {
-		t.Errorf("Pop error: got %v, expected %v", got, expected)
+		if got != expected {
+			t.Errorf("Pop error: got %v, expected %v", got, expected)
+		}
 	}
-
-	// Test Grow
-	stacks.Push(1, 3)
-	stacks.Push(1, 4)
-	stacks.Push(1, 5)
-	stacks.Push(1, 6)
-	stacks.Push(1, 7)
-	stacks.Push(1, 8)
-	stacks.Push(1, 9)
-	stacks.Push(1, 10)
-	stacks.Push(1, 11)
-	fmt.Println(stacks.Storage)
-
-	// Test Shrink
-	fmt.Println(stacks.Pop(1))
-	fmt.Println(stacks.Storage)
-
-	fmt.Println(stacks.Pop(2))
-	fmt.Println(stacks.Storage)
-
-	fmt.Println(stacks.Pop(3))
-	fmt.Println(stacks.Storage)
-
-	fmt.Println(stacks.Pop(1))
-	fmt.Println(stacks.Storage)
-	fmt.Println(stacks.Pop(1))
-	fmt.Println(stacks.Storage)
-	fmt.Println(stacks.Pop(1))
-	fmt.Println(stacks.Storage)
-	fmt.Println(stacks.Pop(1))
-	fmt.Println(stacks.Storage)
-	fmt.Println(stacks.Pop(1))
-	fmt.Println(stacks.Storage)
 }
