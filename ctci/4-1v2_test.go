531c95dc6aad4957bb6a6531a5d2034849a5d9f8
Wally Jones <wally@wallyjones.com>
2020-05-28 22:37:58 -0400

    Add v2 that handles cyclic graphs

---
+++ ctci/4-1v2_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"../data-structures/graph"
+)
+
+func in(n *graph.Node, v []*graph.Node) bool {
+	for _, node := range v {
+		if n == node {
+			return true
+		}
+	}
+
+	return false
+}
+
+func RouteExists(n1, n2 *graph.Node, visited []*graph.Node) bool {
+	if n1 == nil {
+		return false
+	}
+
+	visited = append(visited, n1)
+
+	for _, edge := range n1.Edges {
+		if edge.Node == n2 {
+			return true
+		}
+
+		if !in(edge.Node, visited) {
+			return RouteExists(edge.Node, n2, visited)
+		}
+
+	}
+
+	return false
+}
+
+func TestGraph(t *testing.T) {
+	g := new(graph.Graph)
+
+	n0 := g.AddNode(0)
+	n1 := g.AddNode(1)
+	n2 := g.AddNode(2)
+	n3 := g.AddNode(3)
+	n4 := g.AddNode(4)
+	n5 := g.AddNode(5)
+	n6 := g.AddNode(6)
+
+	n0.AddChild(n1, 1)
+	n1.AddChild(n2, 1)
+	n2.AddChild(n0, 1)
+	n2.AddChild(n4, 1)
+	n3.AddChild(n2, 1)
+	n4.AddChild(n5, 1)
+	n5.AddChild(n6, 1)
+
+	var visited []*graph.Node
+	fmt.Println(RouteExists(n0, n6, visited))
+}
