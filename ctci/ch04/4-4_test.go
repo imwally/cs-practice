b3800c1ca6482e5ff0e8516050312b355069b3c0
Wally Jones <wally@wallyjones.com>
2020-06-12 16:33:01 -0400

    New approach to exercise 4.4

--- ctci/ch04/4-4_test.go
+++ ctci/ch04/4-4_test.go
@@ -18,51 +18,53 @@ func Abs(i int) int {
 	return i
 }
 
-func treeHeight(node *Node) (int, int) {
-	if node == nil {
-		return 0, 0
+func height(current *Node) int {
+	if current == nil {
+		return 0
+
 	}
 
-	l, _ := treeHeight(node.Left)
-	_, r := treeHeight(node.Right)
+	l := height(current.Left)
+	r := height(current.Right)
 
-	l++
-	r++
+	deeper := 0
 
-	return l, r
+	if l > r {
+		deeper = l + 1
+	} else {
+		deeper = r + 1
+	}
+
+	return deeper
 }
 
 func Balanced(node *Node) bool {
-	l, r := treeHeight(node)
+	if node == nil {
+		return true
+	}
 
-	if Abs(l-r) > 1 {
+	lh := height(node.Left)
+	rh := height(node.Right)
+
+	if Abs(lh-rh) > 1 {
 		return false
 	}
 
-	return true
+	return Balanced(node.Left) && Balanced(node.Right)
 }
 
 func TestBalanced(t *testing.T) {
-	root := &Node{1, nil, nil}
-	root.Left = &Node{2, nil, nil}
-	root.Right = &Node{3, nil, nil}
 
-	root.Left.Left = &Node{4, nil, nil}
-	root.Left.Right = &Node{5, nil, nil}
-
-	root.Right.Left = &Node{6, nil, nil}
-	root.Right.Right = &Node{7, nil, nil}
-	root.Right.Right.Right = &Node{8, nil, nil}
-
-	got, expected := Balanced(root), true
+	got, expected := Balanced(nil), true
 	if got != expected {
 		t.Errorf("error: got %v, expected %v", got, expected)
 	}
 
-	// Increase right subtree
-	root.Right.Right.Right.Right = &Node{9, nil, nil}
+	tree := &Node{1, nil, nil}
+	tree.Right = &Node{2, nil, nil}
+	tree.Right.Right = &Node{3, nil, nil}
 
-	got, expected = Balanced(root), false
+	got, expected = Balanced(tree), false
 	if got != expected {
 		t.Errorf("error: got %v, expected %v", got, expected)
 	}
