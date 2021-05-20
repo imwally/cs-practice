b6000b57d5aecb202289aaceb52ec03fb6515b85
Wally Jones <wally@wallyjones.com>
2021-05-19 23:02:01 -0400

    Add tests for AddEdge and Distance methods

--- data-structures/graph/graph_test.go
+++ data-structures/graph/graph_test.go
@@ -16,6 +16,53 @@ func TestGraphAddNode(t *testing.T) {
 	}
 }
 
+func TestGraphAddEdge(t *testing.T) {
+	g := New()
+	n1 := g.AddNode(1)
+	n2 := g.AddNode(2)
+	n3 := g.AddNode(3)
+
+	if err := g.AddEdge(n1, n2, 100); err != nil {
+		t.Error(err)
+	}
+	if err := g.AddEdge(n2, n3, 200); err != nil {
+		t.Error(err)
+	}
+	if err := g.AddEdge(n1, n3, 300); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGraphDistance(t *testing.T) {
+	g := New()
+	n1 := g.AddNode(1)
+	n2 := g.AddNode(2)
+	n3 := g.AddNode(3)
+
+	if err := g.AddEdge(n1, n2, 100); err != nil {
+		t.Error(err)
+	}
+	if err := g.AddEdge(n2, n3, 200); err != nil {
+		t.Error(err)
+	}
+	if err := g.AddEdge(n1, n3, 300); err != nil {
+		t.Error(err)
+	}
+
+	got, expected := n1.Distance(n2), 100
+	if got != expected {
+		t.Errorf("got %d, expected %d", got, expected)
+	}
+	got, expected = n2.Distance(n3), 200
+	if got != expected {
+		t.Errorf("got %d, expected %d", got, expected)
+	}
+	got, expected = n1.Distance(n3), 300
+	if got != expected {
+		t.Errorf("got %d, expected %d", got, expected)
+	}
+}
+
 func TestNodeAddChild(t *testing.T) {
 	g := New()
 	n1 := g.AddNode(1)
